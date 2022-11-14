52e6c46f4616b9e8115dc9846bd065c008b8b50c
Anson <chow.anson25@gmail.com>
2022-11-13 23:01:19 -0500

    anson-Animations

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -22,7 +22,7 @@ const TopSellers = () => {
 
   return (
     <section id="section-popular" className="pb-5">
-      <div className="container">
+      <div className="container" data-aos="fade-in" data-aos-duration="500" data-aos-easing="ease-in">
         <div className="row">
           <div className="col-lg-12">
             <div className="text-center">
